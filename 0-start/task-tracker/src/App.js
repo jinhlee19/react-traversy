f03ea9893256b15bc6f58502eb0c8b1ec7063d53
rafy <jinhlee19@gmail.com>
2022-02-17 22:47:09 +0900

    Step 3 - PropTypes

--- 0-start/task-tracker/src/App.js
+++ 0-start/task-tracker/src/App.js
@@ -1,12 +1,12 @@
-import Header from "./components/Header";
+import Header from './components/Header';
 
 function App() {
 	const name = 'Rafy';
 	const x = false;
 	return (
 		<div className="container">
-		<Header /> 
-		{/* 대문자 Header */}
+			<Header title={'1'} />
+			{/* 대문자 Header */}
 			{/* <h1>Helllo From React</h1>
 			<h2>Hello, {name}</h2>
 			<h3>{1 + 1}</h3>
@@ -14,7 +14,6 @@ function App() {
 		</div>
 	);
 }
-Header.defaultProps = {
-	title: 'Task Tracker',
-}
+// header title 1로 줄때 출력은 되지만 -> Warning: Failed prop type.
+// header.js required 
 export default App;
--- 0-start/task-tracker/src/components/Header.js
+++ 0-start/task-tracker/src/components/Header.js
@@ -1,13 +1,22 @@
-//rafce
+// prop types
+// impt
+import PropTypes from 'prop-types';
 
+//rafce
 // import React from 'react' // 이제 필요없어짐.
 
-const Header = ({title}) => {
-  return (
-    <header>
-        <h1>{title}</h1>
-    </header>
-  )
-}
-
-export default Header
+const Header = ({ title }) => {
+	return (
+		<header>
+			<h1>{title}</h1>
+		</header>
+	);
+};
+Header.defaultProps = {
+	title: 'Task Tracker',
+};
+Header.propTypes = {
+	title: PropTypes.string.isRequired,
+};
+// 대소문자 주의
+export default Header;
