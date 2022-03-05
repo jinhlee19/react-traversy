0a5664508ea50716422fa6922ff6d536c3b71990
rafy <jinhlee19@gmail.com>
2022-03-05 23:11:39 +0900

    Create the Profile Model and Get Current User Profile

---
+++ 1-react/DevConnector/models/Profile.js
@@ -0,0 +1,120 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId, ref: 'user',
+		//user모델의 user의 _id(user모델 안에 보이지는 않지만 부여된 _id)와 연결. ref는 user 모델.
+	},
+	handle: {
+		type: String,
+		required: true,
+		max: 40,
+	},
+	company: {
+		type: String,
+	},
+	website: {
+		type: String,
+	},
+	location: {
+		type: String,
+	},
+	status: {
+		type: String,
+		required: true,
+	},
+	skills: {
+		type: [String],
+		required: true,
+	},
+	bio: {
+		type: String,
+	},
+	githubusername: {
+		type: String,
+	},
+	experience: [
+		{
+			title: {
+				type: String,
+				required: true,
+			},
+			company: {
+				type: String,
+				required: true,
+			},
+			location: {
+				type: String,
+			},
+			from: {
+				type: Date,
+				required: true,
+			},
+			to: {
+				type: Date,
+			},
+			current: {
+				type: Boolean,
+				default: false,
+			},
+			description: {
+				type: String,
+			},
+		},
+	],
+	education: [
+		{
+			school: {
+				type: String,
+				required: true,
+			},
+			degree: {
+				type: String,
+				required: true,
+			},
+			fieldofstudy: {
+				type: String,
+				required: true,
+			},
+			from: {
+				type: Date,
+				required: true,
+			},
+			to: {
+				type: Date,
+			},
+			current: {
+				type: Boolean,
+				default: false,
+			},
+			description: {
+				type: String,
+			},
+		},
+	],
+	social: {
+		youtube: {
+			type: String,
+		},
+		twitter: {
+			type: String,
+		},
+		facebook: {
+			type: String,
+		},
+		linkedin: {
+			type: String,
+		},
+		instagram: {
+			type: String,
+		},
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- 1-react/DevConnector/routes/api/profile.js
+++ 1-react/DevConnector/routes/api/profile.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   Get API/ Profile
-// @desc    Test Route
-// @access  Public
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+// @route   Get api/profile/me (endpoint)
+// @desc    Get current users profile
+// @access  Private
 
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        // Profile 모델의 user 필드의 ObjectId와 연결.
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+	} catch (e) {
+		console.error(e.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
