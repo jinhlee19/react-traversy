2155e245de2bd1f625b8a9c2e1bf6e8f7e107d04
rafy <jinhlee19@gmail.com>
2022-03-11 03:38:13 +0900

    ERR_HTTP_HEADERS_SENT

--- 1-react/DevConnector/routes/api/profile.js
+++ 1-react/DevConnector/routes/api/profile.js
@@ -269,12 +269,22 @@ router.delete('/education/:exp_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
 	try {
 		const options = {
-			uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&clientid=${config.get(
-				'githubClientId'
-			)}&client_secret=${config.get('githubSecret')}`,
+			uri: `https://api.github.com/users/${
+				req.params.username
+			}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get(
+				'githubSecret'
+			)}`,
 			method: 'GET',
-			headers: {'user-agent': 'node.js'}
+			headers: {'user-agent': 'node.js'},
 		};
+		request(options, (error, response, body) => {
+			if (error) console.error(error);
+			if (response.statusCode !== 200) {
+				res.status(404).json({
+					msg: 'No Github Profile Found'});
+			}
+			res.json(JSON.parse(body));
+		});
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server');
