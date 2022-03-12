8d70a0ea76cf835cae7a29d2ca76c0b975bc13c2
rafy <jinhlee19@gmail.com>
2022-03-13 03:43:46 +0900

    Request Example &  Login Form 1 - Example making request

--- 1-react/DevConnector/client/src/components/auth/Register.js
+++ 1-react/DevConnector/client/src/components/auth/Register.js
@@ -1,25 +1,39 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
 	const [formData, setFormData] = useState({ name: '', email: '', password: '', password2: '' });
 	const { name, email, password, password2 } = formData;
 	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
-        if(password !== password2){
-            console.log('Passwords do not match');
-        }else{
-            console.log(formData);
-        }
+	const onSubmit = async e => {
+		if (password !== password2) {
+			console.log('Passwords do not match');
+		} else {
+			// console.log(formData);
+			const newUser = { name, email, password };
+			try {
+				const config = {
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				};
+				const body = JSON.stringify(newUser);
+				const res = await axios.post('/api/users', body, config);
+				console.log(res.data);
+			} catch (err) {
+				console.error(err.response.data);
+			}
+		}
 
-        e.preventDefault();
-    }
+		e.preventDefault();
+	};
 	return (
 		<Fragment>
 			<h1 className="large text-primary">Sign Up</h1>
 			<p className="lead">
 				<i className="fas fa-user"></i> Create Your Account
 			</p>
-			<form className="form" action="create-profile.html" onSubmit={e=>onSubmit(e)}>
+			<form className="form" action="create-profile.html" onSubmit={e => onSubmit(e)}>
 				<div className="form-group">
 					<input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
 				</div>
