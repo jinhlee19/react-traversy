1c774fb1d25bde06bfaf9dcbd2acd7ee7cb5e8fe
rafy <jinhlee19@gmail.com>
2022-03-13 02:20:55 +0900

    React Router Setup

--- 1-react/DevConnector/client/src/App.js
+++ 1-react/DevConnector/client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, {Fragment} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Navbar />
+		<Fragment>
+		<Navbar />
 			<Routes>
-				<Route path="/" element={<Landing/>} />
+				<Route path="/" element={<Landing />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
+		</Fragment>
+			
+			
 		</BrowserRouter>
 	);
 };
 
 export default App;
 
-// 'Fragment is a ghost elements doesn't show up at the DOM'
-// <Route exact path="/" component={Landing} />
 // [https://reactrouter.com/docs/en/v6/getting-started/overview]
+// [https://velog.io/@lilyoh/React-Fragments-%EC%82%AC%EC%9A%A9%EC%9D%B4%EC%9C%A0-%EB%B0%8F-%EC%82%AC%EC%9A%A9%EB%B2%95]
