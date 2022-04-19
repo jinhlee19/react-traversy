59fb69b5760259daf2b96622a7c57d69992ba9ba
rafy <jinhlee19@gmail.com>
2022-04-19 20:46:52 +0900

    memo

--- 1-react/DevConnector/client/src/components/layout/Alert.js
+++ 1-react/DevConnector/client/src/components/layout/Alert.js
@@ -4,6 +4,8 @@
 
 // const Alert = ({ alerts }) =>
 // 	alerts !== null &&
+
+
 // 	alerts.length > 0 &&
 // 	alerts.map(alert => (
 // 		<div
@@ -21,6 +23,8 @@
 // const mapStateToProps = state => ({ alerts: state.alert });
 
 // export default connect(mapStateToProps)(Alert);
+
+// racfp 로 생성
 
 import React from 'react';
 import PropTypes from 'prop-types';
