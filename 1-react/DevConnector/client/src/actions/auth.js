5d5d8af52bbfe6b4f54700cfd145e4393c32bfc2
rafy <jinhlee19@gmail.com>
2022-04-27 06:41:27 +0900

    Logout & Navbar Links

--- 1-react/DevConnector/client/src/actions/auth.js
+++ 1-react/DevConnector/client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 // LOAD USER
@@ -72,3 +72,9 @@ export const login = (email, password) => async dispatch => {
 		});
 	}
 };
+
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+	dispatch({type:LOGOUT})
+}
--- 1-react/DevConnector/client/src/actions/types.js
+++ 1-react/DevConnector/client/src/actions/types.js
@@ -6,5 +6,6 @@ export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS ='LOGIN_SUCCESS';
 export const LOGIN_FAIL ='LOGIN_FAIL';
+export const LOGOUT ='LOGOUT';
 
 // TODO : 아직 이부분도 왜 이렇게 분리해야하는 지 어떻게 뽑아 쓰는건지 이해가 가지 않음.
