5b8a178574cbb2567b6e942019b83609a7060094
rafy <jinhlee19@gmail.com>
2022-05-10 15:43:27 +0900

    좀쉬자

--- 1-react/DevConnector/client/src/App.js
+++ 1-react/DevConnector/client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import PrivateRoute from './routing/PrivateRoute';
 
 import './App.css';
@@ -41,6 +42,7 @@ const App = () => {
 						<Route path="/register" element={<Register />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/profiles" element={<Profiles />} />
+						<Route path="/profile" element={<Profile />} />
 						<Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
 						<Route path="/create-profile" element={<PrivateRoute component={CreateProfile} />} />
 						<Route path="/edit-profile" element={<PrivateRoute component={EditProfile} />} />
---
+++ 1-react/DevConnector/client/src/components/profile/Profile.js
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({ getProfileById, Profile: { profile, loading }, auth, match }) => {
+	useEffect(() => {
+		getProfileById(match.params.id);
+	},[getProfileById]);
+	return <div className='container'>Profile</div>;
+};
+
+Profile.propTypes = {
+	getProfileById: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired,
+	auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+	profile: state.profile,
+	auth: state.auth,
+});
+export default connect(mapStateToProps, { getProfileById })(Profile);
