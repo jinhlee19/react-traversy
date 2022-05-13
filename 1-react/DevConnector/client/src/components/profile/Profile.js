4b74cd034aeb7c696ad71fc883166c07b07fb4c4
rafy <jinhlee19@gmail.com>
2022-05-13 12:33:56 +0900

    Spinner Change

--- 1-react/DevConnector/client/src/components/profile/Profile.js
+++ 1-react/DevConnector/client/src/components/profile/Profile.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import { useParams } from 'react-router-dom';
 
-const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
+const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
+	const { id } = useParams();
 	useEffect(() => {
-		getProfileById(match.params.id);
-	}, []);
+		getProfileById(id);
+	}, [getProfileById, id]);
 
 	return (
 		<div className="container">
@@ -31,3 +33,37 @@ const mapStateToProps = state => ({
 
 // connect = prop을 담아옴. 최소 프로필 데이터 연결, 그후 getProfileById 부분은 action 부분
 export default connect(mapStateToProps, { getProfileById })(Profile);
+
+// import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import Spinner from '../layout/Spinner';
+// import { getProfileById } from '../../actions/profile';
+
+// const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
+// 	useEffect(() => {
+// 		getProfileById(match.params.id);
+// 	}, [getProfileById]);
+
+// 	return (
+// 		<div className="container">
+// 			Profile
+// 			<Spinner />
+// 		</div>
+// 	);
+// };
+
+// Profile.propTypes = {
+// 	getProfileById: PropTypes.func.isRequired,
+// 	profile: PropTypes.object.isRequired,
+// 	auth: PropTypes.object.isRequired,
+// };
+
+// const mapStateToProps = state => ({
+// 	profile: state.profile,
+// 	// 로그인 했을경우, edit profile 버튼을 보여주기 위해 auth 상태를 불러옴.
+// 	auth: state.auth,
+// });
+
+// // connect = prop을 담아옴. 최소 프로필 데이터 연결, 그후 getProfileById 부분은 action 부분
+// export default connect(mapStateToProps, { getProfileById })(Profile);
