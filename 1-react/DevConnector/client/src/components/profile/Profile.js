e6aef9a95d1ba024892dc1c93c1d55176e969d2c
rafy <jinhlee19@gmail.com>
2022-05-13 20:11:02 +0900

    Profile Top, About, Experience, and Education End

--- 1-react/DevConnector/client/src/components/profile/Profile.js
+++ 1-react/DevConnector/client/src/components/profile/Profile.js
@@ -6,6 +6,9 @@ import { getProfileById } from '../../actions/profile';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 	const { id } = useParams();
@@ -29,6 +32,28 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 					)}
 					<div class="profile-grid my-1">
 						<ProfileTop profile={profile} />
+						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map(experience => (
+										<ProfileExperience key={experience._id} experience={experience} />
+									))}
+								</Fragment>
+							):(<h4>No Experience Credentials</h4>)}
+						</div>
+
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map(education => (
+										<ProfileEducation key={education._id} education={education} />
+									))}
+								</Fragment>
+							):(<h4>No Education Credentials</h4>)}
+						</div>
 					</div>
 				</Fragment>
 			)}
--- 1-react/DevConnector/client/src/components/profile/ProfileTop.js
+++ 1-react/DevConnector/client/src/components/profile/ProfileTop.js
@@ -1,51 +1,102 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ProfileTop = ({
-  profile: {
-    status,
-    company,
-    location,
-    website,
-    social,
-    user: { name, avatar }
-  }
+	profile: {
+		status,
+		company,
+		location,
+		website,
+		social,
+		user: { name, avatar },
+	},
 }) => {
-  return (
-    <div className="profile-top bg-primary p-2">
-      <img className="round-img my-1" src={avatar} alt="" />
-      <h1 className="large">{name}</h1>
-      <p className="lead">
-        {status} {company ? <span> at {company}</span> : null}
-      </p>
-      <p>{location ? <span>{location}</span> : null}</p>
-      <div className="icons my-1">
-        {website ? (
-          <a href={website} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-globe fa-2x" />
-          </a>
-        ) : null}
-        {social
-          ? Object.entries(social)
-              .filter(([_, value]) => value)
-              .map(([key, value]) => (
-                <a
-                  key={key}
-                  href={value}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className={`fab fa-${key} fa-2x`}></i>
-                </a>
-              ))
-          : null}
-      </div>
-    </div>
-  );
+	return (
+		<div className="profile-top bg-primary p-2">
+			<img className="round-img my-1" src={avatar} alt="" />
+			<h1 className="large">{name}</h1>
+			<p className="lead">
+				{status} {company ? <span> at {company}</span> : null}
+			</p>
+			<p>{location && <span>{location}</span>}</p>
+			<div className="icons my-1">
+				{website && (
+					<a href={website} target="_blank" rel="noopener noreferrer">
+						<i className="fas fa-globe fa-2x" />
+					</a>
+				)}
+				{social && social.twitter && (
+					<Link to="#" target="_blank" rel="noopener noreferrer">
+						<i class="fab fa-twitter fa-2x"></i>
+					</Link>
+				)}
+                {social && social.facebook && (
+					<Link to={social.facebook}  target="_blank" rel="noopener noreferrer">
+						<i class="fab fa-facebook fa-2x"></i>
+					</Link>
+				)}
+                {social && social.youtube && (
+					<Link to={social.youtube} target="_blank" rel="noopener noreferrer">
+						<i class="fab fa-youtube fa-2x"></i>
+					</Link>
+				)}
+                {social && social.linkedin && (
+					<Link to={social.linkedin} target="_blank" rel="noopener noreferrer">
+						<i class="fab fa-linkedin fa-2x"></i>
+					</Link>
+				)}
+                {social && social.instagram && (
+					<Link to={social.instagram}  target="_blank" rel="noopener noreferrer">
+						<i class="fab fa-instagram fa-2x"></i>
+					</Link>
+				)}
+			</div>
+		</div>
+	);
 };
 
 ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired
+	profile: PropTypes.object.isRequired,
 };
 
 export default ProfileTop;
+
+// const ProfileTop = ({
+// 	profile: {
+// 		status,
+// 		company,
+// 		location,
+// 		website,
+// 		social,
+// 		user: { name, avatar },
+// 	},
+// }) => {
+// 	return (
+// 		<div className="profile-top bg-primary p-2">
+// 			<img className="round-img my-1" src={avatar} alt="" />
+// 			<h1 className="large">{name}</h1>
+// 			<p className="lead">
+//                 {status} {company && <span> at {company}</span>}
+// 				{status} {company ? <span> at {company}</span> : null}
+// 			</p>
+// 			<p>{location ? <span>{location}</span> : null}</p>
+// 			<div className="icons my-1">
+// 				{website ? (
+// 					<a href={website} target="_blank" rel="noopener noreferrer">
+// 						<i className="fas fa-globe fa-2x" />
+// 					</a>
+// 				) : null}
+// 				{social
+// 					? Object.entries(social)
+// 							.filter(([_, value]) => value)
+// 							.map(([key, value]) => (
+// 								<a key={key} href={value} target="_blank" rel="noopener noreferrer">
+// 									<i className={`fab fa-${key} fa-2x`}></i>
+// 								</a>
+// 							))
+// 					: null}
+// 			</div>
+// 		</div>
+// 	);
+// };
