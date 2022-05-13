b7073150104abc2aa8667a6375fe07b0cdf76455
rafy <jinhlee19@gmail.com>
2022-05-13 18:13:22 +0900

    ProfileTop 1 - Trouble Shooting

--- 1-react/DevConnector/client/src/components/profile/Profile.js
+++ 1-react/DevConnector/client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 	const { id } = useParams();
@@ -17,17 +18,20 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 			{profile === null || loading ? (
 				<Spinner />
 			) : (
-        <Fragment>
-          <Link to="/profiles" className="btn btn-light">
-            Back To Profiles
-          </Link>
-          {auth.isAuthenticated &&
-            auth.loading === false &&
-            auth.user._id === profile.user._id && (
-              <Link to="/edit-profile" className="btn btn-dark">
-                Edit Profile
-              </Link>
-            )}</Fragment>)}
+				<Fragment>
+					<Link to="/profiles" className="btn btn-light">
+						Back To Profiles
+					</Link>
+					{auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+						<Link to="/edit-profile" className="btn btn-dark">
+							Edit Profile
+						</Link>
+					)}
+					<div class="profile-grid my-1">
+						<ProfileTop profile={profile} />
+					</div>
+				</Fragment>
+			)}
 		</div>
 	);
 };
@@ -46,39 +50,3 @@ const mapStateToProps = state => ({
 
 // connect = prop을 담아옴. 최소 프로필 데이터 연결, 그후 getProfileById 부분은 action 부분
 export default connect(mapStateToProps, { getProfileById })(Profile);
-
-//// Backup
-
-// import React, { useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-// import Spinner from '../layout/Spinner';
-// import { getProfileById } from '../../actions/profile';
-
-// const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
-// 	useEffect(() => {
-// 		getProfileById(match.params.id);
-// 	}, [getProfileById]);
-
-// 	return (
-// 		<div className="container">
-// 			Profile
-// 			<Spinner />
-// 		</div>
-// 	);
-// };
-
-// Profile.propTypes = {
-// 	getProfileById: PropTypes.func.isRequired,
-// 	profile: PropTypes.object.isRequired,
-// 	auth: PropTypes.object.isRequired,
-// };
-
-// const mapStateToProps = state => ({
-// 	profile: state.profile,
-// 	// 로그인 했을경우, edit profile 버튼을 보여주기 위해 auth 상태를 불러옴.
-// 	auth: state.auth,
-// });
-
-// // connect = prop을 담아옴. 최소 프로필 데이터 연결, 그후 getProfileById 부분은 action 부분
-// export default connect(mapStateToProps, { getProfileById })(Profile);
