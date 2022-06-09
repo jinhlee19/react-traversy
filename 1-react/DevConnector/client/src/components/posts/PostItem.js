835551d681effc9a7629f8949bf5826f4067d009
jinhlee19 <jinhlee19@gmail.com>
2022-06-10 07:29:43 +0900

    Deleting Posts

--- 1-react/DevConnector/client/src/components/posts/PostItem.js
+++ 1-react/DevConnector/client/src/components/posts/PostItem.js
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => (
+const PostItem = ({
+	addLike,
+	removeLike,
+	deletePost,
+	auth,
+	post: { _id, text, name, avatar, user, likes, comments, date },
+}) => (
 	<div className="post bg-white p-1 my-1">
 		<div>
 			<Link to={`/profile/${_id}`}>
@@ -30,7 +36,7 @@ const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, 
 				Discussion {comments.length > 0 && <span className="comment-count">{comments.length}</span>}
 			</Link>
 			{!auth.loading && user === auth.user._id && (
-				<button type="button" className="btn btn-danger">
+				<button onClick={e => deletePost(_id)} type="button" className="btn btn-danger">
 					<i className="fas fa-times"></i>
 				</button>
 			)}
@@ -41,6 +47,9 @@ const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, 
 PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired,
+	addLike: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -50,4 +59,4 @@ const mapStateToProps = state => ({
 });
 
 // Delete버튼을 유저에게만 적용하기 위해서 auth를 불러옴.
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem);
