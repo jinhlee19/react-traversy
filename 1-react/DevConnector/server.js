95f66ef4c24608948d701926317033f07d7502c3
jinhlee19 <jinhlee19@gmail.com>
2022-06-11 05:35:09 +0900

    Prepare For Deployment 2

--- 1-react/DevConnector/server.js
+++ 1-react/DevConnector/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,13 +11,22 @@ connectDB();
 app.use(express.json({ extended: false }));
 // querystring 모듈을 사용하여 쿼리스트링을 해석 true는 qs(외부패키지)
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 //this will look for an environment variable called port to use and when we deploy to hiroku, that's where its going to get the port.
