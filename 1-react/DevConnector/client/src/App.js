256144e51c8e415842f46a1c3c582196d80a96a7
jinhlee19 <jinhlee19@gmail.com>
2022-06-10 22:46:41 +0900

    Single Post Display

--- 1-react/DevConnector/client/src/App.js
+++ 1-react/DevConnector/client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import PrivateRoute from './routing/PrivateRoute';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import Test from './components/layout/Test';
 import './App.css';
 
@@ -50,6 +51,7 @@ const App = () => {
 						<Route path="/add-experience" element={<PrivateRoute component={AddExperience} />} />
 						<Route path="/add-education" element={<PrivateRoute component={AddEducation} />} />
 						<Route path="/posts" element={<PrivateRoute component={Posts} />} />
+						<Route path="/posts/:id" element={<PrivateRoute component={Post} />} />
 						<Route path="/test" element={<Test />} />
 					</Routes>
 				</Fragment>
