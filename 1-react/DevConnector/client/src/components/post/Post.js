50f42460d7ffdf6dfcd0df7f9bae59fbf810dc34
jinhlee19 <jinhlee19@gmail.com>
2022-06-11 02:59:28 +0900

    Comment Display & Delete

--- 1-react/DevConnector/client/src/components/post/Post.js
+++ 1-react/DevConnector/client/src/components/post/Post.js
@@ -5,18 +5,29 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
 	const { id } = useParams();
 	useEffect(() => {
 		getPost(id);
 	}, [getPost, id]);
+
 	return loading || post === null ? (
 		<Spinner />
 	) : (
 		<div className="container">
-			<Link to=""></Link>
+			<Link to="/posts" className="btn">
+				Back To Posts
+			</Link>
 			<PostItem post={post} showActions={false} />
+			<CommentForm postId={post._id} />
+			<div className="comments">
+				{post.comments.map(comment => (
+					<CommentItem key={comment._id} comment={comment} postId={post._id} />
+				))}
+			</div>
 		</div>
 	);
 };
--- 1-react/DevConnector/client/src/reducers/post.js
+++ 1-react/DevConnector/client/src/reducers/post.js
@@ -1,4 +1,13 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from '../actions/types';
+import {
+	GET_POSTS,
+	POST_ERROR,
+	UPDATE_LIKES,
+	DELETE_POST,
+	ADD_POST,
+	GET_POST,
+	ADD_COMMENT,
+	REMOVE_COMMENT,
+} from '../actions/types';
 
 const initialState = {
 	posts: [],
@@ -37,7 +46,18 @@ export default function (state = initialState, action) {
 				posts: state.posts.filter(post => post._id !== payload),
 				loading: false,
 			};
-
+		case ADD_COMMENT:
+			return {
+				...state,
+				post: { ...state.post, comments: payload },
+				loading: false,
+			};
+		case REMOVE_COMMENT:
+			return {
+				...state,
+				comments: state.post.comments.filter(comment => comment._id !== payload),
+				loading: false,
+			};
 		case POST_ERROR:
 			return {
 				...state,
