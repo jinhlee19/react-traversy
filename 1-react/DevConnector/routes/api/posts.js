9cfd5dfedfd355ab868e75f7818ce788e8131536
jinhlee19 <jinhlee19@gmail.com>
2022-06-14 04:54:22 +0900

    production test 1

--- 1-react/DevConnector/routes/api/posts.js
+++ 1-react/DevConnector/routes/api/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const { restart } = require('nodemon');
 
 const auth = require('../../middleware/auth');
 
